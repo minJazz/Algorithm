d931a9b6efb44c65fc30a14435dbcfe75c7f94b1
MinJazz <alswo9588@gmail.com>
2021-02-04 16:55:40 +0900

    Create 백준2588

--- src/입출력과사칙연산/Q11_2588.java
+++ src/입출력과사칙연산/Q11_2588.java
@@ -1,5 +1,16 @@
 package 입출력과사칙연산;
 
-public class Q11_2588 {
+import java.util.Scanner;
 
+public class Q11_2588 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a*(b%10));
+        System.out.println(a*((b/10)%10));
+        System.out.println(a*((b/100)%10));
+        System.out.println(a*b);
+    }
 }
