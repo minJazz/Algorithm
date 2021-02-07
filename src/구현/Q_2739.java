ef579396b12c752de27ad32b2d713cb4c6be81c3
MinJazz <alswo9588@gmail.com>
2021-02-07 23:05:23 +0900

    Create 구현

---
+++ src/구현/Q_2739.java
@@ -0,0 +1,11 @@
+package 구현;
+
+import java.util.Scanner;
+
+public class Q_2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }
+}
